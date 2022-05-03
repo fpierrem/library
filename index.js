ca4c3c83c020f117dd5fa8bc93c9a39de04d3de4
florian <florian@florians-MacBook-Pro-2.local>
2022-05-03 18:18:32 +0200

    update index.js

--- index.js
+++ index.js
@@ -10,27 +10,33 @@ function Book(title,author,genre,year,numPages) {
 
 const testBook = new Book('My Life','John','Memoirs',2010,100)
 
+console.log(testBook);
+
 function addBookToLibrary(book) {
   myLibrary.push(book)
 }
 
 addBookToLibrary(testBook);
 
+console.log(myLibrary);
+
 function displayBooks() {
     const books = document.getElementById('books');
-    for (book in myLibrary) {
-        const card = document.createElement('div');
-        card.className = "card";
-        card.innerHTML =
-          `
-          <div class="title">${book.title}</div>
-          <div class="author">${book.author}</div>
-          <div class="genre">${book.genre}</div>
-          <div class="year">${book.year}</div>
-          <div class="numPages">${book.numPages}</div>
-          `;    
-        books.append(card);
+    myLibrary.forEach(displayBook);
+    function displayBook(book) {
+      console.log(book);
+      const card = document.createElement('div');
+      card.className = "card";
+      card.innerHTML =
+        `
+        <div class="title">${book.title}</div>
+        <div class="author">${book.author}</div>
+        <div class="genre">${book.genre}</div>
+        <div class="year">${book.year}</div>
+        <div class="numPages">${book.numPages}</div>
+        `;    
+      books.append(card);
     }
-}
+  }
 
 document.addEventListener('DOMContentLoaded', displayBooks)
